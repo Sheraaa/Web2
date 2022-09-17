a1d25902ff3a109fb97a5eacbcc835dadc1bbefb
Sheraaa <chehrazad.ouazzani.student.vinci.be>
2022-09-17 22:02:30 +0200

    ex 1.2

---
+++ exercises/module1/1.2/index.js
@@ -0,0 +1,18 @@
+const btn = document.querySelector("button");
+const clockHolder = document.querySelector("span");
+
+btn.addEventListener("click", stopOrResumeClock);
+
+var myIntervalId;
+
+startClock();
+
+function startClock() {
+  myIntervalId = setInterval(printCurrentTime, 1000);
+}
+
+function printCurrentTime() {
+  const now = new Date();
+  const time = now.toLocaleTimeString();
+  clockHolder.innerText = time;
+}
