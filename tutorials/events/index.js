3d46fce62b9b0330070218309f85e7f5e589ef41
Sheraaa <chehrazad.ouazzani.student.vinci.be>
2022-09-17 20:32:09 +0200

    tutorials

--- tutorials/events/index.js
+++ tutorials/events/index.js
@@ -1,49 +1,19 @@
-const btn1 = document.querySelector("#myBtn1");
+const divs = document.querySelectorAll(".message");
 
-const btn2 = document.querySelector("#myBtn2");
+divs.forEach((div) => {
+  div.addEventListener("mouseover", () => {
+    div.innerText = "Hello world!";
+  });
 
-const buttons = document.querySelectorAll("button");
-
-const btn3 = buttons[2]; // fancy way to get a reference...
-
-const btn4 = document.querySelector("#myBtn4");
-
-
-btn1.addEventListener("click", () => {
-
-  btn1.innerText = "I have been clicked !";
-
-  console.log("onClickHandlerForBtn1::click");
-
+  div.addEventListener("mouseout", (shera) => {
+    div.innerText = `You have left the div wit id ${shera.target.id}`;
+  });
 });
+// code to get the reference to the form shall be imagined
 
-btn2.addEventListener("click", onClickHandlerForBtn2);
-
-btn2.addEventListener("click", onClickHandlerForBtnExtra);
-
-btn3.addEventListener("click", onClickHandlerForBtn3);
-
-
-function onClickHandlerForBtn2() {
-
-  btn2.innerText = "I have also been clicked";
-
-  console.log("onClickHandlerForBtn2::click");
-
-}
-
-
-function onClickHandlerForBtn3() {
-
-  console.log("onClickHandlerForBtn3::click");
-
-  btn2.removeEventListener("click", onClickHandlerForBtnExtra);
-
-}
-
-
-function onClickHandlerForBtnExtra() {
-
-  console.log("onClickHandlerForBtnExtra::click");
-
-}
+const onSubmit = (shera) => {
+    console.log("onSubmit::");
+    shera.preventDefault();
+  };
+  
+  myForm.addEventListener("submit", onSubmit);  
