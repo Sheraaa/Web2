dee0ecee67f40022c4806127577c37efcd4202c9
Chehrazad <chehrazad.ouazzani@student.vinci.be>
2022-09-19 12:51:32 +0200

    1.4

--- exercises/module1/1.4/index.js
+++ exercises/module1/1.4/index.js
@@ -2,15 +2,38 @@ let timeoutID;
 const delayInSeconds = 2;
 const delayInMiliSeconds = delayInSeconds * 1000;
 
+const divRed = document.querySelector('.red');
+const divOrange = document.querySelector('.orange');
+const divGreen = document.querySelector('.green');
+
+while(true){
+  divGreen.style.backgroundColor = "white";
+  divRed.style.backgroundColor = "red";
+  startClock();
+  divRed.style.backgroundColor = "white";
+  divOrange.style.backgroundColor = "orange";
+  startClock();
+  divOrange.style.backgroundColor = "white";
+  divGreen.style.backgroundColor = "green";
+}
 function delayedAlert() {
   timeoutID = setTimeout(() => {
     
   }, delayInMiliSeconds);
 }
+let myIntervalId;
 
-divs.forEach((div) => {
-    
-});
+function startClock() {
+  myIntervalId = setInterval(printCurrentTime, 1000);
+}
+
+function stopOrResumeClock() {
+  if (myIntervalId) {
+    clearInterval(myIntervalId);
+    myIntervalId = undefined;
+  } else startClock();
+
+}
 
 function clearAlert() {
   clearTimeout(timeoutID);
