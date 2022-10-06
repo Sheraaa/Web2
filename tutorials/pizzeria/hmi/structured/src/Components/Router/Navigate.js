4a9b4d0db42ad4e81b20a839ea8b1ecaeb0520fd
Chehrazad <chehrazad.ouazzani@student.vinci.be>
2022-10-06 12:45:55 +0200

    pooppooopop

--- tutorials/pizzeria/hmi/structured/src/Components/Router/Navigate.js
+++ tutorials/pizzeria/hmi/structured/src/Components/Router/Navigate.js
@@ -1,6 +1,6 @@
 const Navigate = (toUri) => {
     const fromUri = window.location.pathname;
-    if (fromUri === toUri) return;
+    if (fromUri === toUri) return;  // si chemin
   
     window.history.pushState({}, '', toUri);
     const popStateEvent = new PopStateEvent('popstate', { state: {} });
