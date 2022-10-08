eca2d8f135c2eb9c1de0bacb70adb50c07345fed
Sheraaa <chehrazad.ouazzani@student.vinci.be>
2022-10-08 21:44:46 +0200

    myMovies : module 3 : refactoring & MooviePage

---
+++ exercises/module3/3.1a/MyMovies/src/Components/Router/routes.js
@@ -0,0 +1,12 @@
+import AddMoviePage from '../Pages/AddMoviePage'
+import HomePage from '../Pages/HomePage'
+import ViewMoviepage from '../Pages/ViewMoviePage'
+
+
+const routes = {
+    '/': HomePage,
+    '/view-movies': ViewMoviepage,
+    '/add-movie': AddMoviePage,
+};
+
+export default routes;
