066079ec070e83ffdc5f6793f89a8fcb5aa35015
Sheraaa <chehrazad.ouazzani@student.vinci.be>
2022-10-13 07:17:36 +0200

    api-basic not done

---
+++ tutorials/pizzeria/hmi/api/basic/app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var usersRouter = require('./routes/users');
+var pizzaRouter = require('./routes/pizzas');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/pizzas', pizzaRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
